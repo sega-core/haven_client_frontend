2615e4f11d250330193df3e36f5897680e578d09
Sergey Kornienko <kornienko955@gmail.com>
2026-02-11 16:27:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { TabBar } from "./components/TabBar";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./modules/Header";
 import { QueryClientProvider, AppRoutes, DrawerProvider } from "./containers";
-import { init } from "@tma.js/sdk-react";
+import { init, postEvent } from "@tma.js/sdk-react";
 
 function App() {
   try {
     init();
+    postEvent('web_app_setup_swipe_behavior', { allow_vertical_swipe: false })
   } catch (error) {
     console.log(error);
   }
