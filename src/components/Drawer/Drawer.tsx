5066cd5392b76012f24b443dda58a2590b8cdaf6
Sergey Kornienko <kornienko955@gmail.com>
2026-02-12 16:55:31 +0300

    Update Drawer.tsx

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,7 +1,7 @@
 // components/DrawerDesktopFix.tsx
 import { Drawer as VaulDrawer } from "vaul";
 import { Typography } from "../Typography";
-import { useEffect, useRef, forwardRef, useImperativeHandle } from "react";
+import { forwardRef } from "react";
 import cn from "../../utils/cn";
 
 interface DrawerProps {
@@ -14,34 +14,7 @@ interface DrawerProps {
 }
 
 export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
-  ({ title, children, open, onClose, isNasted, className }, ref) => {
-    const contentRef = useRef<HTMLDivElement>(null);
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-    // Прокидываем ref наружу
-    useImperativeHandle(ref, () => contentRef.current as HTMLDivElement);
-
-    useEffect(() => {
-      if (!open || isMobile) return;
-
-      const handleClickOutside = (e: MouseEvent) => {
-        if (
-          contentRef.current &&
-          !contentRef.current.contains(e.target as Node)
-        ) {
-          onClose();
-        }
-      };
-
-      const timer = setTimeout(() => {
-        document.addEventListener("click", handleClickOutside);
-      }, 100);
-
-      return () => {
-        clearTimeout(timer);
-        document.removeEventListener("click", handleClickOutside);
-      };
-    }, [open, isMobile, onClose]);
+  ({ title, children, open, onClose, isNasted, className }) => {
 
     const DrawerComponent = isNasted ? VaulDrawer.NestedRoot : VaulDrawer.Root;
 
@@ -51,9 +24,6 @@ export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
         onOpenChange={(isOpen) => {
           if (!isOpen) onClose();
         }}
-         shouldScaleBackground={false}
-         noBodyStyles={true} 
-        dismissible={isMobile}
       >
         <VaulDrawer.Portal>
           <VaulDrawer.Overlay
@@ -64,30 +34,18 @@ export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
           />
 
           <VaulDrawer.Content
-            ref={contentRef}
             className={cn(
               "bg-white-primary rounded-t-4xl flex flex-col fixed bottom-0 left-0 right-0 max-h-[90vh] outline-none",
               className,
               `${!isNasted ? "z-41" : "z-43"}`,
             )}
-            onPointerDownOutside={(e) => {
-              if (!isMobile) {
-                e.preventDefault();
-              }
-            }}
           >
             <div className="flex justify-center p-2">
               <div className="w-12 h-1.5 rounded-full bg-beige-primary" />
             </div>
-
-                    {JSON.stringify(isMobile)}
-                    ---
-        {JSON.stringify(navigator.userAgent)}
-
             <div
               className={cn(
-                "p-4 flex-1 overflow-auto gap-4 flex flex-col",
-                !isMobile ? "pt-6" : "",
+                "p-4 flex-1 overflow-auto gap-4 flex flex-col pt-6",
               )}
             >
               {title && (
