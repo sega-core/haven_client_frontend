c9cf403d28a59ee471f8ac960557dd3d5d3fe819
Sergey Kornienko <kornienko955@gmail.com>
2026-05-12 15:27:10 +0300

    Update HeroUiProvider.tsx

--- src/containers/HeroUiProvider/HeroUiProvider.tsx
+++ src/containers/HeroUiProvider/HeroUiProvider.tsx
@@ -1,10 +1,8 @@
 import { HeroUIProvider } from "@heroui/system";
-import { ToastProvider } from "@heroui/toast";
 
 export const HeroUiProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <HeroUIProvider>
-      <ToastProvider placement="top-center" />
       {children}
     </HeroUIProvider>
   );
